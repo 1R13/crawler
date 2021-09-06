da82bdb569e5bb9ced3ca96e7eca000a19c4c1d8
zer0 <57137168+6E7511@users.noreply.github.com>
2021-09-06 17:53:46 +0200

    Cleaning and search implemented

--- main.py
+++ main.py
@@ -1,49 +1,29 @@
 import src
-import datetime
 
 
-Todo = []
-Todo2 = []
+root = input("Enter your root URL : ")
+level = int(input("Enter the max depth to seach : "))
+restrain = input("Enter restrain on which urls shall be searched : ")
+term = input("Enter a serach term if you desire : ") or None
 
-Found = []
+session = src.WebScraper(root, level, restrain)
+if term:
+    session.search_term = term
 
-Searched = []
-# URLS = []
 
-target = int(input("How many levels for searching? :"))
-level = 1
+def test():
+    return src.WebScraper("https://google.com/", 2, "google.com")
 
-Todo.append(input("Enter first URL i.e. https://www.google.com : "))
-limit = input("Limit to URLs containing : ") or "."
-started = datetime.datetime.now()
-# lives = int(input("How many levels ? "))
 
-while level <= target:
-    Todo2 = []
-    for url in Todo:
-        try:
-            Found = src.Src.web_scrape(url, limit)
-        except Exception as err:
-            print("\n", err, "at", url)
-        for u in Found:
-            if not Todo.__contains__(u) and not Searched.__contains__(u) and not Todo2.__contains__(u):
-                Todo2.append(u)
-                # print(lives)
-        Searched.append(url)
-        print("Finished ", url)
-        print(len(Searched), "Websites searched,", len(Todo)
-              , "Websites on todo,", len(Todo2), "on todo2,", "at level", level
-              , "/", target, "[" + str(datetime.datetime.now() - started) + "]", end="\r")
-        Todo.remove(url)
-    Todo += Todo2
-    level += 1
-    # print("\n", len(Todo), "items on todo list.")
-finished = datetime.datetime.now()
+session.start()
+session.scrape_loop()
 
-for u in Found:
-    print(u)
-
-print(50*"-", "\n", len(Searched), "unique urls searched,", target, "levels searched,", len(Todo) + len(Todo2), "URLs open, took", finished - started)
-
-# print(hits, "successful search hits.")
-
+"""for u in session.found_urls:
+    if u.__contains__("google.com"):
+        print(u)
+for u in session.found_urls:
+    if not u.__contains__("google.com"):
+        print(u)
+"""
+for url in session.search_hits:
+    print(url)
--- src.py
+++ src.py
@@ -1,39 +1,105 @@
 import requests
+import datetime
 
 
-class Src:
+class WebScraper:
 
-    def web_scrape(url, limit: str):
+    # Initializes Session with provided parameters
+    def __init__(self, root_url, level: int, restrain: str):
+        self.search_hits = []
+        self.found_urls = []
+        self.root_url = root_url
+        self.level = level
+        self.time_start = datetime.datetime.now()
+        self.restrain = restrain
+        self.step = 0
+        self.todo1 = []
+        self.todo2 = []
+        self.search_term = None
 
-        hoopla_counter = 0
+    # Searches a string for http and https urls and returns them
+    def get_urls_from_line(self, line):
 
+        urls = []
+
+        i = line.find("http")
+        e = i
+        while line.find("http", i) != -1 and e <= len(line) and i <= len(line):  # finds first pattern match
+            i = line.find("http", i)  # and sets end of string
+            e = i  # further by scanning it
+            try:  # for escape characters
+                while not "\" <>(){}\'[]*;\\".__contains__(line[e]):
+                    e += 1
+            except Exception as err:
+                print(err)
+            u = line[i:e]
+            if not urls.__contains__(u) and not u.__contains__("\/") and not u.__contains__("https?") \
+                    and u.__contains__(".") and u.__contains__("://"):
+                urls.append(u)
+            i = e
+        return urls
+
+    def start(self):
         try:
-            r = requests.get(url)
+            r = requests.get(self.root_url)
+            print("Received url_source.")
 
-            # print(r.text)
-            urls = []
-
-            for line in r.text.splitlines():    # iterates through html request line by line
-
+            for line in r.text.splitlines():    # Iterates line by line through source_code
                 if line.__contains__("http"):
-                    i = line.find("http")
-                    e = i
-                    while line.find("http", i) != -1 and e <= len(line) and i <= len(line): # finds first pattern match
-                        i = line.find("http", i)                                            # and sets end of string
-                        e = i                                                               # further by scanning it
-                        try:                                                                # for escape characters
-                            while not "\" <>(){}\'[]*;\\".__contains__(line[e]):
-                                e += 1
-                        except Exception as err:
-                            # print(err)
-                            hoopla_counter += 1
-                        u = line[i:e]
-                        if not urls.__contains__(u) and not u.__contains__("\/") and not u.__contains__("https?") \
-                                and u.__contains__(".") and u.__contains__("://") and u.__contains__(limit):
-                            urls.append(u)
-                        i = e
+                    urls = self.get_urls_from_line(line)
+                    for url in urls:
+                        if not self.found_urls.__contains__(url):   # Adds new urls to found and todo1 list
+                            self.found_urls.append(url)
+                            # print(url)
+                            if url.__contains__(self.restrain):
+                                self.todo1.append(url)
 
-            return urls     # returns all found URLs
+                try:
+                    if line.__contains__(self.search_term) and not self.search_hits.__contains__(self.root_url):
+                        print(line)
+                        self.search_hits.append(self.root_url)
+                except TypeError:
+                    if False:
+                        print("No search term set.")
+
         except requests.exceptions.ConnectionError:
-            return []
+            print("Cant connect to given url.")
 
+    def scrape_loop(self):
+        while self.step < self.level:
+            for url in self.todo1:
+
+                r = requests.get(url)
+                for line in r.text.splitlines():
+
+                    if line.__contains__("http"):
+
+                        urls = self.get_urls_from_line(line)
+                        for u in urls:
+                            if not self.found_urls.__contains__(u):
+                                self.found_urls.append(u)
+                                # print(url)
+                                if not self.todo1.__contains__(u) and not self.todo2.__contains__(u) and \
+                                        u.__contains__(self.restrain):
+                                    self.todo2.append(u)
+
+                    try:
+                        if line.__contains__(self.search_term) and not self.search_hits.__contains__(url):
+                            self.search_hits.append(url)
+                            if len(line) < 200:
+                                print(line)
+                    except TypeError:
+                        if False:
+                            print("No search term set.")
+                self.todo1.remove(url)
+            self.todo1 += self.todo2
+            self.todo2 = []
+            self.step += 1
+
+
+"""class Hit:
+
+    def __init__(self, url, line):
+        self.url = url
+        self.line = line
+"""
